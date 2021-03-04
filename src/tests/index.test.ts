c69af22f281c6d03c7f9cbf1c0cf79bdb12a9d73
Saksham Khurana <dawnimpulse@gmail.com>
2021-03-04 18:13:48 +0530

    test: for keys and map fn

--- src/tests/index.test.ts
+++ src/tests/index.test.ts
@@ -6,7 +6,7 @@ import appRoot from "app-root-path";
 import { writeFileSync, unlinkSync } from "fs";
 import { resolve } from "path";
 import { expect } from "chai";
-import { init, keys } from "../app";
+import { init, keys, map } from "../app";
 
 describe("with defaults testing", () => {
     describe("+ve tests", () => {
@@ -29,9 +29,46 @@ describe("with defaults testing", () => {
             init();
         });
 
-        // contain 4 keys
-        it("expect module to contain 4 keys", function () {
-            expect(Object.keys(keys())).to.have.lengthOf(4);
+        // keys functions
+        describe("keys", () => {
+            // contain 4 keys
+            it("expect keys fn to return 4 keys", function () {
+                expect(Object.keys(keys())).to.have.lengthOf(4);
+            });
+
+            // contain same keys as given
+            it("expect to return same keys", function () {
+                const originalKeys = [
+                    "error1",
+                    "error101",
+                    "internal1",
+                    "internal201",
+                ];
+                expect(Object.keys(keys())).to.eql(originalKeys);
+            });
+
+            // key value should match expected result
+            it("expect values to match", function () {
+                const values = [4001011, 4001012, 5001021, 5001022];
+                expect(Object.values(keys())).to.eql(values);
+            });
+        });
+
+        // map function
+        describe("map", () => {
+            let map1;
+
+            before(() => {
+                map1 = map();
+            });
+
+            // map should match expected
+            it("expect map to match", function () {
+                expect(map1).to.eql({
+                    400: { error1: 4001011, error101: 4001012 },
+                    500: { internal1: 5001021, internal201: 5001022 },
+                });
+            });
         });
 
         // delete config file from root
